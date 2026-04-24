8e33e2728a68a24132033c8e5efc22eb47fa0a31
300111ttk <300111ttk@gmail.com>
2026-04-24 14:19:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    GEMINI_API_KEY: "AIzaSyBpLKzttfjlZCopWefPWx7GAPv_RL9n-ag" // Dán API Key thật của bạn vào đây
+    PROXY_URL: "https://script.google.com/macros/s/AKfycbzDoNm5mfvjWI1RcVny36HS2rM3sEtfOcDQAi_IJu-FN9ACklVD75qAp2TmdKBFvzpJUg/exec"
 };
