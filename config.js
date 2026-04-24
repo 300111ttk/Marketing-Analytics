2c71addefd0e25161aa2856face309983363f9e5
300111ttk <300111ttk@gmail.com>
2026-04-24 16:34:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 const CONFIG = {
-    // Địa chỉ Local Server từ LM Studio
-    LOCAL_API_URL: "http://127.0.0.1:1234/v1/chat/completions",
-    // Tên model bạn đang dùng trong LM Studio
-    MODEL_NAME: "qwen2.5-coder-7b-instruct" 
+    GEMINI_API_KEY: "AIzaSyA3sWE3V8Oqxk3kgG9IQJECVtf14rrdQzo",
+    GEMINI_API_URL: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent"
 };
