5ea499f4d11db6de70f5594b5678b1e4578a2274
300111ttk <300111ttk@gmail.com>
2026-04-24 14:07:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    GEMINI_API_KEY: "AIzaSy..." // Dán API Key thật của bạn vào đây
+    GEMINI_API_KEY: "AIzaSyBpLKzttfjlZCopWefPWx7GAPv_RL9n-ag" // Dán API Key thật của bạn vào đây
 };
