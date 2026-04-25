c8672aa79f5df8139d22228bcea9de323947e2c6
300111ttk <300111ttk@gmail.com>
2026-04-25 08:16:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const CONFIG = {
-    GEMINI_API_KEY: "AIzaSyA3sWE3V8Oqxk3kgG9IQJECVtf14rrdQzo",
+    // Key này lấy chính xác từ ảnh API key details của bạn
+    GEMINI_API_KEY: "AIzaSyCZijDLLtPEPLa32FmbG_DIYQsf0TD1Wvc",
     GEMINI_API_URL: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent"
 };
